3208241e2e3eef993820b082d7c5b70b87ff2753
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-07-06 23:49:28 -0500

    Add pick_number function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import random
+
+
+def pick_number() -> int:
+    return random.randint(1, 100)
 
 
 def main():
-    pass
+    cops_num = pick_number()
 
 
 if __name__ == '__main__':
