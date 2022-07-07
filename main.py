e00cb2f3f84d484e58bcfa6eb9fd0a8be0afc37a
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-07-06 23:50:42 -0500

    Add get player_number() function

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def pick_number() -> int:
     return random.randint(1, 100)
 
 
+def player_number():
+    return int(input("Enter your guess: "))
+
+
 def main():
     cops_num = pick_number()
+    users_num = player_number()
 
 
 if __name__ == '__main__':
