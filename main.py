03203c7c65c883a4c66d5e476bf0b4622f909cee
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-07-07 00:15:20 -0500

    Fix linting errors

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ def player_number() -> int:
 
 def check_number(computer, player):
     if player < computer:
-        return 'LOW'
+        return "LOW"
     elif player > computer:
-        return 'HIGH'
-    return 'WIN'
+        return "HIGH"
+    return "WIN"
 
 
 def main():
     comps_num = pick_number()
     users_num = player_number()
     num_status = check_number(comps_num, users_num)
-    print(f'Your Guess: {num_status}')
+    print(f"Your Guess: {num_status}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
     sys.exit(main())
